64180dd6bb7d9980532744238c436f7b529045b2
7lucckyy <7lucckyy6611@gmail.com>
2022-06-24 11:08:26 +0100

    change to production

--- config/db.js
+++ config/db.js
@@ -1,9 +1,14 @@
 const sequelize = require('sequelize');
 
-const db = new sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.HOSTNAME,
-    dialect: "postgres"
-})
+const db = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
 
 module.exports = db;
