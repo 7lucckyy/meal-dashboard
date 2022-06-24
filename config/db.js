a5018c5090bd6fc1312334b8b12e68847f39217f
7lucckyy <7lucckyy6611@gmail.com>
2022-06-24 11:13:55 +0100

    sequelise updated

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL, {
+const db = new sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
       ssl: {
         require: true,
