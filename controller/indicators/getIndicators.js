b7bf7c39bdb494e4122772a08db6fc817053b543
7lucckyy <7lucckyy6611@gmail.com>
2022-06-30 23:07:31 +0100

    getAll Indicators and log

--- controller/indicators/getIndicators.js
+++ controller/indicators/getIndicators.js
@@ -4,13 +4,14 @@ const Projects = require('../../model/project-infos');
 module.exports = async (req, res)=>{
     try {
         let IndicatorCode = req.body.indicator_code;
-        const getAllIndicators = Indicators.findAll({
+        const getAllIndicators = await Indicators.findAll({
             where: {
-                
+                is_deleted: 0
             }
         })
+        return console.log({getAllIndicators});
         
     } catch (e) {
-        
+        console.log(e);
     }
 }
