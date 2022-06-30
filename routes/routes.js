bc69973f8af65c847cc24a6eda720248c26f3818
7lucckyy <7lucckyy6611@gmail.com>
2022-06-30 23:10:40 +0100

    get indicators to routes folder

--- routes/routes.js
+++ routes/routes.js
@@ -9,6 +9,7 @@ const postUser = require('../controller/user/postUser');
 const postLogin = require('../controller/user/loginAuth');
 const postProjectInfo = require('../controller/project/postProject');
 const postIndicators = require('../controller/indicators/postIndicators');
+const getIndicators = require('../controller/indicators/getIndicators');
 
 
 
@@ -21,6 +22,7 @@ router.post('/register',  postUser);
 router.post('/login', postLogin);
 router.post('/create', postProjectInfo);
 router.post('/create-indicator', postIndicators);
+router.post('/get-indicators', getIndicators);
 
 
 
