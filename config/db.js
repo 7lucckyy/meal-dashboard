e17f980fa850e7ca831b2b06ff42768f467dbc17
7lucckyy <7lucckyy6611@gmail.com>
2022-07-03 14:38:29 +0100

    add db.js config to remote db and remove all files uploaded

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const sequelize = require('sequelize');
+
+const db = new sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
+
+
+
+module.exports = db;
