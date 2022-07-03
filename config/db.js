6b8a2f25a3d1b3ee1955425fe2929f1291e6f8be
7lucckyy <7lucckyy6611@gmail.com>
2022-07-03 21:45:04 +0100

    db local add development and fixed

--- config/db.js
+++ config/db.js
@@ -1,14 +1,22 @@
 const sequelize = require('sequelize');
 
-const db = new sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
+let db = null;
+if(process.env.DB_ENV == 'development'){
+  db = new sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres'
+})
+}else{
+  db  = new Sequelize(process.env.DATABASE_URL, {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
       }
     }
-  }
-);
+  );
+}
 
 
 
--- controller/indicators/getIndicators.js
+++ controller/indicators/getIndicators.js
@@ -2,19 +2,24 @@ const Indicators = require('../../model/indicators');
 const Projects = require('../../model/project-infos');
 const sequelize = require('sequelize');
 const db = require('../../config/db')
+
 module.exports = async (req, res)=>{
     try {
         let IndicatorCode = req.body.indicator_code;
-        const getAllIndicators = await Indicators.findAll({
-            attributes: [
-             sequelize.fn('sum', sequelize.col('child_boys'),)],
-            raw: true
+        // const getAllIndicators = await Indicators.findAll({
+        //     attributes: [
+        //      sequelize.fn('sum', sequelize.col('child_boys'),)],
+        //      raw: true
+        // })
+        const getAllIndicatorsSum = await db.query('SELECT sum(child_boys) AS total_child_boys, sum(child_girls) AS total_child_girls FROM indicators', {
+            type: sequelize.QueryTypes.SELECT
         })
-        if(getAllIndicators){
-            return res.status(200).json({
-                getAllIndicators
-            })
-        }
+        console.log(getAllIndicatorsSum);
+        // if(getAllIndicators){
+        //     return res.status(200).json({
+        //        data: getAllIndicators
+        //     })
+        // }
         
     } catch (e) {
         console.log(e);
